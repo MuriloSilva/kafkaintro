71bed4d832e3ae0fc4b6689e237d66b4685915c9
Murilo Rodrigues de Paula <murilo.silva@r2tec.net>
2019-05-06 14:00:02 -0300

    ajuste package de testes

---
+++ src/test/java/com/mumudeveloper/kafkaexample/KafkaintroApplicationTests.java
@@ -0,0 +1,21 @@
+package com.mumudeveloper.kafkaexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KafkaintroApplicationTests {
+	
+	private Logger logger = LoggerFactory.getLogger("log");
+
+    @Test
+    public void contextLoads() {
+    	logger.info("Só {} Murilo de {}", "vai", "Paula");
+    }
+
+}
